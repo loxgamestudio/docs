8ae9bf9d93f5d0c27ebe25cf286f2f1df74d19fd
LordOfXen <38406718+LordOfXen@users.noreply.github.com>
2018-10-28 12:21:56 -0700

    Add files via upload

---
+++ apis/suspiciousdomainchecker/js/lox_languages.js
@@ -0,0 +1,15 @@
+/* 
+* Copyright (c) 2018 - Lox Game Studio. All rights reserved.
+* Developed by: LordOf Xen
+*/
+
+/*
+* Generated with Lox Javascript Generator from Turkish to English.
+*/
+var english =
+{
+    "title": "Lox Game Studio",
+    "copyright": "Copyright &copy; 2018 - Lox Game Studio. All rights reserved.<br>All trademarks are the property of their respective owners in the US and other countries."
+};
+
+var turkish = null;
